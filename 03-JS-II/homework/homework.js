d0b7ef0f5074b7434a9d510527068ffb785a8d4c
Alex <alex27loza@gmail.com>
2022-07-30 17:33:53 -0400

    tareajs2

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -166,7 +166,7 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  let tablaDelSeis = [];
+  var tablaDelSeis = [];
   for(let i = 0; i <= 10; i++){
     let tabla = 6 * i;
     tablaDelSeis.push(tabla);
